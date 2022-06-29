974d08a82821f705a4f9143c0e153dbef7abf1d5
Asia84 <84asia@mail.ru>
2022-06-30 00:43:10 +0700

    решение task15

---
+++ task15/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace DayWEEk
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"ВВедите число дня недели: ");
+            int number = Convert.ToInt32(Console.ReadLine());   
+
+            Console.WriteLine(GetWeekday(number));
+        }
+
+        static string GetWeekday( int num )
+        {
+            if( num > 0 && num <= 5)
+            {
+                return $"Сегодня рабочий день";
+            }
+            else if(num > 5 && num < 8)
+            {
+                return $"Сегодня выходной";
+            }
+            else
+            {
+                return $"Введено неверное число";
+            }
+        }
+    }
+}
+
+
+
+
+
+

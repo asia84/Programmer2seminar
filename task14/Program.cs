51e323e78afbe4ae67548cfd1a0393e5507c8131
Asia84 <84asia@mail.ru>
2022-06-29 19:23:27 +0700

    решила 14 задачу с семинара сама, ура

---
+++ task14/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Напишите программу, которая принимает на вход число и проверяет, 
+кратно ли оно одновременно 7 и 23. 
+14 -> нет 46 -> нет 161 -> да */
+
+//int randomNumber = new Random().Next(1, 1000);//определяет рандомное число
+Console.Write("Введите число, чтобы определить его кратность: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+Console.WriteLine (number); //(randomNumber);
+    if (number % (7 *23) == 0)
+    {
+    
+        Console.WriteLine($"Число {number} кратно числу 7 и 23");
+    }
+
+    else
+    {
+        result = number % (7*23);
+        Console.WriteLine ($"Число {number} не кратно числам 7 и 23");
+    }    
+
+
+
+
+
+
+
+
+
+
+
